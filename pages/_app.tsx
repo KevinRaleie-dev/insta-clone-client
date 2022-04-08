c0d5cd9bd1942a83934f52fd9e74258a5c7c485e
KevinRaleie-dev <kkevraleie@gmail.com>
2022-04-08 13:20:07 +0200

    added auth headers to apollo client

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,27 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { ApolloClient, ApolloProvider, createHttpLink, InMemoryCache } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+import type { AppProps } from 'next/app';
+import { getToken } from '../store';
+import '../styles/globals.css';
 
-import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client'
+
+const authLink = setContext((_, { headers }) => {
+  const token = getToken('token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : ''
+    }
+  }  
+});
+
+const httpLink = createHttpLink({
+  uri: 'http://localhost:4000/graphql'
+})
 
 const client = new ApolloClient({
   cache: new InMemoryCache(),
-  uri: 'http://localhost:4000/graphql',
+  link: authLink.concat(httpLink)
 });
 
 function MyApp({ Component, pageProps }: AppProps) {
